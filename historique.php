c527b7cc56df2bc5b99193cddd4410ef8b650cd1
GuillaumeMEY <guillaume.meyer@viacesi.fr>
2023-01-31 17:06:00 +0100

    MAJ front index + commencer front page2
    maj de l'index et debut du front de la page historique

---
+++ historique.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css" />
+    <title>Historque</title>
+
+</head>
+<body class="body2">
+    <div class="header">
+        <div class="logo">Meteo.</div>
+        <div class="date">Jeudi 28 Janvier</div>
+    </div>
+
+
+    <div class="historique">
+    <div class="villehistorique">Pau</div>
+        <div class="listehistorique">
+
+            <p class="phistorique">Jeudi 28 Janvier</p>
+                <div class="deroulant"> 
+                    <div class="tempexte">
+                        <h3>Température exterieur</h3>
+                        <p class="grp">Temp Max 27 °C</p>
+                        <p class="grp">Temp Min 20 °C</p>
+                        <p class="grp">Temp Moyenne 22°C</p>
+
+                    </div>
+                    <div class="pression">
+                        <p class="grp1">Humidité Moyenne 50%</p>
+                        <p class="grp1">Pression Moyenne 1005 hPa</p>
+                    </div>
+                    <div class="tempinte">
+                    <h3>Température Capteur</h3>
+                        <p class="grp">Temp Max 27 °C</p>
+                        <p class="grp">Temp Min 20 °C</p>
+                        <p class="grp">Temp Moyenne 22°C</p>
+                    
+                    </div>
+                    <p class="grp2">+</p>
+                    <div class="suite"></div>
+                </div>
+            <p class="phistorique">Jeudi 28 Janvier</p>
+            <p class="phistorique">Jeudi 28 Janvier</p>
+        </div>
+    </div>
+    <div class="aboutus">About Us</div>
+</body>
+</html>
