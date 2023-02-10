580118d6c9b5281d5be41c4b93fbfd777aac0207
nicolas balao <nicolasbalao.pro@gmail.com>
2023-02-10 12:20:55 +0000

    Add init for db and the script for api

---
+++ main.py
@@ -0,0 +1,165 @@
+import mariadb
+import sys
+from typing import Union
+import requests
+import json
+import datetime
+
+"""
+    Todo:
+       - Refactor the code for POO
+       - Type all function ...
+       - Refactor name function to snake_case
+"""
+
+def connectDatabase():
+    try:
+        conn = mariadb.connect(
+            user="cesi",
+            password="cesi",
+            host="localhost",
+            port=3306,
+            database="weather"
+
+        )
+        print("Connection initialised with mariadb")
+    except mariadb.Error as e:
+        print("Error connecting to Mariadb: {0}".format(e))
+        sys.exit(1)
+
+    return conn, conn.cursor()
+
+   
+def handleGetCityData(city_data, _db : mariadb.Cursor) -> Union[float, float, int]:
+    isData, lat, lon, id = getCityDateFromDB(city_data.name, _db)
+    if isData:
+        return lat, lon, id
+    id = storeNewCityInDB(city_data["name"], city_data["lat"], city_data["lon"], _db)
+    return lat, lon, id
+
+
+
+def getCityDateFromDB(city_name : str, _db : mariadb.Cursor) -> Union[bool, float, float, int]:
+    try:
+        query = "SELECT lat, lon, id FROM city WHERE name=%s"
+        data = (city_name,)
+        _db.execute(query, data)
+        row = _db.fetchall()
+        if not row:
+            return False, 0, 0, 0
+
+        return True, row[0][0], row[0][1], row[0][2]
+
+    except mariadb.Error as e:
+        print("Error retrieving entry from database: {0}".format(e))
+    return False, 0, 0, 0
+
+def getCityDataFromApi(city_name) -> Union[float, float]:
+    api_url = "http://api.openweathermap.org/geo/1.0/direct?q={0}&limit=5&appid={1}".format(city_name, "0554aa483aa31c4b4aa9cf79d18542e1")
+    response = requests.get(api_url)
+    data = json.loads(response.text)
+    return data[0]["lat"], data[0]["lon"]
+
+def storeNewCityInDB(city_name, city_lat, city_lon, _db) -> int:
+    try:
+        query = "INSERT INTO city (lat, lon, name) values(?, ?, ?)"
+        data = (city_lat, city_lon, city_name)
+        _db.execute(query, data)
+        print("City data stored")
+        return _db.lastrowid
+    except mariadb.Error as e:
+        print("Error inserting data in city table: {0}".format(e))
+
+    
+def getWeatherApi(city_lat, city_lon):
+    
+    url = "https://api.openweathermap.org/data/2.5/weather?lat={0}&lon={1}&appid={2}&units=metric".format(city_lat, city_lon, "0554aa483aa31c4b4aa9cf79d18542e1")
+    response = requests.get(url)
+
+    data = json.loads(response.text)
+    return data
+
+def storeWeatherApiInDB(api_data, city_id, day_id, _db):
+    try:
+        query = "INSERT INTO api_weather(city_id, weather, temp, humidity, pressure, day_id) values(?,?,?,?,?,?)"
+        data = (city_id, api_data["weather"][0]["main"], api_data["main"]["temp"], api_data["main"]["humidity"], api_data["main"]["pressure"], day_id)
+        _db.execute(query, data)
+        print("Api weather data stored")
+    except mariadb.Error as e:
+        print("Error inserting data in api_weather: {0}".format(e))
+
+def getCurrentDayId(_db : mariadb.Cursor) -> int:
+    try: 
+        query = "SELECT id FROM day WHERE date=?"
+        data = (datetime.date.today(),)
+        _db.execute(query, data)
+        row = _db.fetchall()
+        if  not row:
+            id = createNewDay(_db)
+            return id
+        id = row[0][0]
+        return id
+    except mariadb.Error as e:
+        print("Error retrieving data from day table: {0}".format(e))
+
+
+def createNewDay(_db : mariadb.Cursor) -> int:
+    try:
+        query = "INSERT INTO day(date) VALUES (?)"
+        data = (datetime.date.today(),)
+        _db.execute(query, data)
+        print("New day created")
+        id = _db.lastrowid
+        return id
+    except mariadb.Error as e:
+        print("Error create day: {0}".format(e))
+
+
+def handleGeolocation():
+    ip_addr = getPublicIp()
+    city_data = locatePublicIp(ip_addr)
+
+    return city_data
+
+
+#Get the pubic ip
+def getPublicIp() -> str:
+    resp = requests.get('https://api64.ipify.org?format=json').json()
+    print(resp)
+    return resp["ip"]
+
+# Locate the ip add
+def locatePublicIp(ip_addr : str):
+    resp =requests.get(f'https://ipapi.co/{ip_addr}/json/').json()
+    city_data = {
+        "name": resp.get("city"),
+        "lat": resp.get("latitude"),
+        "lon": resp.get("longitude")
+    }
+
+    print("city_data", city_data)
+
+    return city_data
+#uxaq1861eu2jtf3j api key https://ipregistry.co/
+
+#8898a6c67188d817f2fba8a72419b1a4 https://www.whatismyip.com/login-welcome-page/
+
+
+def main():
+
+    conn, db = connectDatabase()
+
+    city_data = handleGeolocation()
+
+    day_id = getCurrentDayId(db)
+
+    city_lat, city_lon, city_id = handleGetCityData(city_data, db)
+
+    weather_api = getWeatherApi(city_lat, city_lon)
+    storeWeatherApiInDB(weather_api, city_id, day_id, db)
+    # Push the change into the db
+    conn.commit()
+    
+
+if __name__ == '__main__':
+    main()
