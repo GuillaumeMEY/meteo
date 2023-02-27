9a6508153a21895829e11f7f5fbc60c810320f71
nicolas balao <nicolasbalao.pro@gmail.com>
2023-02-27 16:25:34 +0000

    Wip(script): The script work need to clean up the main file

--- script/api_geo_loc.py
+++ script/api_geo_loc.py
@@ -3,20 +3,19 @@ import requests
 class ApiGeoLoc:
 
     def __init__(self):
-        self.api64_url = "https://api64.ipify.org?",
-        self.ipapi_url = "'https://ipapi.co/"
+        self.ipapi_url = "https://ipapi.co"
     
     def get_public_addr(self):
         try:
-            resp = requests.get('{0}format=json'.format(self.api64_url)).json()
+            resp = requests.get("https://api64.ipify.org?format=json").json()
             print("Getting the public")
             return resp["ip"]
         except requests.ConnectionError as e:
             print("Error while connection: {0}".format(e))
     
-    def get_city_by_ip(self, ip):
+    def get_city_by_ip(self, ip_addr):
         try:
-            resp =requests.get('{0}{1}/json/'.format(ip, self.ipapi_url)).json()
+            resp =requests.get(f"{self.ipapi_url}/{ip_addr}/json/").json()
             city_data = {
                 "name": resp.get("city"),
                 "lat": resp.get("latitude"),
--- script/main.py
+++ script/main.py
@@ -15,137 +15,137 @@ from api_geo_loc import ApiGeoLoc
        - write requirement txt
 """
 
-def connectDatabase():
-    try:
-        conn = mariadb.connect(
-            user="cesi",
-            password="cesi",
-            host="localhost",
-            port=3306,
-            database="weather"
+# def connectDatabase():
+#     try:
+#         conn = mariadb.connect(
+#             user="cesi",
+#             password="cesi",
+#             host="localhost",
+#             port=3306,
+#             database="weather"
 
-        )
-        print("Connection initialised with mariadb")
-    except mariadb.Error as e:
-        print("Error connecting to Mariadb: {0}".format(e))
-        sys.exit(1)
+#         )
+#         print("Connection initialised with mariadb")
+#     except mariadb.Error as e:
+#         print("Error connecting to Mariadb: {0}".format(e))
+#         sys.exit(1)
 
-    return conn, conn.cursor()
+#     return conn, conn.cursor()
 
    
-def handleGetCityData(city_data, _db : mariadb.Cursor) -> Union[float, float, int]:
-    isData, lat, lon, id = getCityDateFromDB(city_data.name, _db)
-    if isData:
-        return lat, lon, id
-    id = storeNewCityInDB(city_data["name"], city_data["lat"], city_data["lon"], _db)
-    return lat, lon, id
+# def handleGetCityData(city_data, _db : mariadb.Cursor) -> Union[float, float, int]:
+#     isData, lat, lon, id = getCityDateFromDB(city_data.name, _db)
+#     if isData:
+#         return lat, lon, id
+#     id = storeNewCityInDB(city_data["name"], city_data["lat"], city_data["lon"], _db)
+#     return lat, lon, id
 
 
 
-def getCityDateFromDB(city_name : str, _db : mariadb.Cursor) -> Union[bool, float, float, int]:
-    try:
-        query = "SELECT lat, lon, id FROM city WHERE name=%s"
-        data = (city_name,)
-        _db.execute(query, data)
-        row = _db.fetchall()
-        if not row:
-            return False, 0, 0, 0
+# def getCityDateFromDB(city_name : str, _db : mariadb.Cursor) -> Union[bool, float, float, int]:
+#     try:
+#         query = "SELECT lat, lon, id FROM city WHERE name=%s"
+#         data = (city_name,)
+#         _db.execute(query, data)
+#         row = _db.fetchall()
+#         if not row:
+#             return False, 0, 0, 0
 
-        return True, row[0][0], row[0][1], row[0][2]
+#         return True, row[0][0], row[0][1], row[0][2]
 
-    except mariadb.Error as e:
-        print("Error retrieving entry from database: {0}".format(e))
-    return False, 0, 0, 0
+#     except mariadb.Error as e:
+#         print("Error retrieving entry from database: {0}".format(e))
+#     return False, 0, 0, 0
 
-def getCityDataFromApi(city_name) -> Union[float, float]:
-    api_url = "http://api.openweathermap.org/geo/1.0/direct?q={0}&limit=5&appid={1}".format(city_name, "0554aa483aa31c4b4aa9cf79d18542e1")
-    response = requests.get(api_url)
-    data = json.loads(response.text)
-    return data[0]["lat"], data[0]["lon"]
+# def getCityDataFromApi(city_name) -> Union[float, float]:
+#     api_url = "http://api.openweathermap.org/geo/1.0/direct?q={0}&limit=5&appid={1}".format(city_name, "0554aa483aa31c4b4aa9cf79d18542e1")
+#     response = requests.get(api_url)
+#     data = json.loads(response.text)
+#     return data[0]["lat"], data[0]["lon"]
 
-def storeNewCityInDB(city_name, city_lat, city_lon, _db) -> int:
-    try:
-        query = "INSERT INTO city (lat, lon, name) values(?, ?, ?)"
-        data = (city_lat, city_lon, city_name)
-        _db.execute(query, data)
-        print("City data stored")
-        return _db.lastrowid
-    except mariadb.Error as e:
-        print("Error inserting data in city table: {0}".format(e))
+# def storeNewCityInDB(city_name, city_lat, city_lon, _db) -> int:
+#     try:
+#         query = "INSERT INTO city (lat, lon, name) values(?, ?, ?)"
+#         data = (city_lat, city_lon, city_name)
+#         _db.execute(query, data)
+#         print("City data stored")
+#         return _db.lastrowid
+#     except mariadb.Error as e:
+#         print("Error inserting data in city table: {0}".format(e))
 
     
-def getWeatherApi(city_lat, city_lon):
+# def getWeatherApi(city_lat, city_lon):
     
-    url = "https://api.openweathermap.org/data/2.5/weather?lat={0}&lon={1}&appid={2}&units=metric".format(city_lat, city_lon, "0554aa483aa31c4b4aa9cf79d18542e1")
-    response = requests.get(url)
+#     url = "https://api.openweathermap.org/data/2.5/weather?lat={0}&lon={1}&appid={2}&units=metric".format(city_lat, city_lon, "0554aa483aa31c4b4aa9cf79d18542e1")
+#     response = requests.get(url)
 
-    data = json.loads(response.text)
-    return data
+#     data = json.loads(response.text)
+#     return data
 
-def storeWeatherApiInDB(api_data, city_id, day_id, _db):
-    try:
-        query = "INSERT INTO api_weather(city_id, weather, temp, humidity, pressure, day_id) values(?,?,?,?,?,?)"
-        data = (city_id, api_data["weather"][0]["main"], api_data["main"]["temp"], api_data["main"]["humidity"], api_data["main"]["pressure"], day_id)
-        _db.execute(query, data)
-        print("Api weather data stored")
-    except mariadb.Error as e:
-        print("Error inserting data in api_weather: {0}".format(e))
+# def storeWeatherApiInDB(api_data, city_id, day_id, _db):
+#     try:
+#         query = "INSERT INTO api_weather(city_id, weather, temp, humidity, pressure, day_id) values(?,?,?,?,?,?)"
+#         data = (city_id, api_data["weather"][0]["main"], api_data["main"]["temp"], api_data["main"]["humidity"], api_data["main"]["pressure"], day_id)
+#         _db.execute(query, data)
+#         print("Api weather data stored")
+#     except mariadb.Error as e:
+#         print("Error inserting data in api_weather: {0}".format(e))
 
-def getCurrentDayId(_db : mariadb.Cursor) -> int:
-    try: 
-        query = "SELECT id FROM day WHERE date=?"
-        data = (datetime.date.today(),)
-        _db.execute(query, data)
-        row = _db.fetchall()
-        if  not row:
-            id = createNewDay(_db)
-            return id
-        id = row[0][0]
-        return id
-    except mariadb.Error as e:
-        print("Error retrieving data from day table: {0}".format(e))
-
-
-def createNewDay(_db : mariadb.Cursor) -> int:
-    try:
-        query = "INSERT INTO day(date) VALUES (?)"
-        data = (datetime.date.today(),)
-        _db.execute(query, data)
-        print("New day created")
-        id = _db.lastrowid
-        return id
-    except mariadb.Error as e:
-        print("Error create day: {0}".format(e))
+# def getCurrentDayId(_db : mariadb.Cursor) -> int:
+#     try: 
+#         query = "SELECT id FROM day WHERE date=?"
+#         data = (datetime.date.today(),)
+#         _db.execute(query, data)
+#         row = _db.fetchall()
+#         if  not row:
+#             id = createNewDay(_db)
+#             return id
+#         id = row[0][0]
+#         return id
+#     except mariadb.Error as e:
+#         print("Error retrieving data from day table: {0}".format(e))
 
 
-def handleGeolocation():
-    ip_addr = getPublicIp()
-    city_data = locatePublicIp(ip_addr)
+# def createNewDay(_db : mariadb.Cursor) -> int:
+#     try:
+#         query = "INSERT INTO day(date) VALUES (?)"
+#         data = (datetime.date.today(),)
+#         _db.execute(query, data)
+#         print("New day created")
+#         id = _db.lastrowid
+#         return id
+#     except mariadb.Error as e:
+#         print("Error create day: {0}".format(e))
 
-    return city_data
+
+# def handleGeolocation():
+#     ip_addr = getPublicIp()
+#     city_data = locatePublicIp(ip_addr)
+
+#     return city_data
 
 
-#Get the pubic ip
-def getPublicIp() -> str:
-    resp = requests.get('https://api64.ipify.org?format=json').json()
-    print(resp)
-    return resp["ip"]
+# #Get the pubic ip
+# def getPublicIp() -> str:
+#     resp = requests.get('https://api64.ipify.org?format=json').json()
+#     print(resp)
+#     return resp["ip"]
 
-# Locate the ip add
-def locatePublicIp(ip_addr : str):
-    resp =requests.get(f'https://ipapi.co/{ip_addr}/json/').json()
-    city_data = {
-        "name": resp.get("city"),
-        "lat": resp.get("latitude"),
-        "lon": resp.get("longitude")
-    }
+# # Locate the ip add
+# def locatePublicIp(ip_addr : str):
+#     resp =requests.get(f'https://ipapi.co/{ip_addr}/json/').json()
+#     city_data = {
+#         "name": resp.get("city"),
+#         "lat": resp.get("latitude"),
+#         "lon": resp.get("longitude")
+#     }
 
-    print("city_data", city_data)
+#     print("city_data", city_data)
 
-    return city_data
-#uxaq1861eu2jtf3j api key https://ipregistry.co/
+#     return city_data
+# #uxaq1861eu2jtf3j api key https://ipregistry.co/
 
-#8898a6c67188d817f2fba8a72419b1a4 https://www.whatismyip.com/login-welcome-page/
+# #8898a6c67188d817f2fba8a72419b1a4 https://www.whatismyip.com/login-welcome-page/
 
 """
     Algo:
@@ -191,31 +191,32 @@ def main():
     # Declare and init Class
     database = Database()
     api_weather = ApiWeather()
-    # api_geo_loc = ApiGeoLoc()
+    api_geo_loc = ApiGeoLoc()
     
 
-    # my_ip = api_geo_loc.get_public_addr()
-    # my_city = api_geo_loc.get_city_by_ip(my_ip)
-    
-    # Get latitude and logitude of the city for the OpenWeatherMap api
-    city_lat, city_lon = api_weather.get_city_data("pau")
-    
-    # Get the current weather data of the city 
-    api_weather_response = api_weather.get_weather_data(city_lat, city_lon)
+    my_ip = api_geo_loc.get_public_addr()
+    my_city = api_geo_loc.get_city_by_ip(my_ip)
 
+    # # Get latitude and logitude of the city for the OpenWeatherMap api
+    # city_lat, city_lon = api_weather.get_city_data("pau")
+    
+    # # Get the current weather data of the city 
+    api_weather_response = api_weather.get_weather_data(my_city["lat"], my_city["lon"])
+
+    print(api_weather_response)
     weather_data = data_extraction(api_weather_response)
     
     
     data = [
-        ("temp", weather_data['temp'], "api"),
-        ("humidity", weather_data['humidity'], "api"),
-        ("pression", weather_data['pressure'], "api"),
-        ("weather", weather_data['weather'], "api")
-    ]
+         ("temp", weather_data['temp'], "api"),
+         ("humidity", weather_data['humidity'], "api"),
+         ("pression", weather_data['pressure'], "api"),
+         ("weather", weather_data['weather'], "api")
+     ]
     
     database.insert_into_Measures(data)
     
-    # For push modification into the database
+    # # For push modification into the database
     database.con.commit()
 
     database.close_connection()
