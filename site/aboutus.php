809de9afbccdd4fe9c004d0dea965372db25c2a6
guimey <guillaume.meyer@viacesi.fr>
2023-03-13 14:35:44 +0100

    correctif css

---
+++ site/aboutus.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com"> <!--  font -->
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> <!--  font -->
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet"> <!--  font -->
+    <link rel="stylesheet" href="style.css" /> <!--  route vers le fichier css-->
+    <title>About Us</title>
+</head>
+<body class="body1">
+    <div class="about">
+        <div class="photogui">
+            
+        </div>
+        <div class="text">
+            Travail au Departement des Pyrénées-Atlantiques <br>
+            Développeur back end <br>
+            Technologie: Laravel, Javascript 
+        </div>
+
+    </div>
+    <div class="about">
+
+        <div class="text">
+            Travail chez YUKAN INNOVATE <br>
+            Développeur back end <br>
+            Technologie: Laravel, Javascript 
+        </div>
+        <div class="photonico">
+            
+        </div>
+
+    </div>
+    <div class="about">
+        <div class="photodav">
+            
+        </div>
+        <div class="text">
+            Travail chez CGI <br>
+            Développeur back end <br>
+            Technologie: Laravel, Javascript 
+        </div>
+
+    </div>
+</body>
+</html>
